78229a2820a5f8c33ef559ece8384ff220efdf1c
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-04-27 20:46:54 +0200

    UTF8 encoding, install with uv, remove fs.link

--- install.js
+++ install.js
@@ -13,17 +13,14 @@ module.exports = {
     {
       method: "shell.run",
       params: {
+        env: {
+          "PYTHONUTF8": "1"
+        },
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install litellm flask",
+          "uv pip install litellm flask",
         ]
-      }
-    },
-    {
-      method: "fs.link",
-      params: {
-        venv: "app/env"
       }
     }
   ]
