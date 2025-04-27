a4644fd8cd3eb23ad71e1e71f16fa0f76050e28e
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-04-27 20:52:12 +0200

    save disk space, script version 3.7

--- pinokio.js
+++ pinokio.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  version: "2.0",
+  version: "3.7",
   title: "ditto",
   description: "the simplest self-building coding agent https://github.com/yoheinakajima/ditto",
   icon: "icon.jpeg",
@@ -11,6 +11,7 @@ module.exports = {
       start: info.running("start.js"),
       update: info.running("update.js"),
       reset: info.running("reset.js"),
+      link: info.running("link.js"),
       n: info.running("new.js")
     }
     if (running.install) {
@@ -56,6 +57,13 @@ module.exports = {
           text: "Resetting",
           href: "reset.js",
         }]
+      } else if (running.link) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Deduplicating",
+          href: "link.js",
+        }]
       } else if (running.n) {
         return [{
           default: true,
@@ -82,9 +90,15 @@ module.exports = {
           text: "Install",
           href: "install.js",
         }, {
+          icon: "fa-solid fa-file-zipper",
+          text: "<div><strong>Save Disk Space</strong><div>Deduplicates redundant library files</div></div>",
+          href: "link.js",
+        }, {
           icon: "fa-regular fa-circle-xmark",
-          text: "Reset",
+          text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
           href: "reset.js",
+          confirm: "Are you sure you wish to reset the app?"
+
         }]
       }
     } else {
